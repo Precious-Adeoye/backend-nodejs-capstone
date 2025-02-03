a3d7a3b71c2cd0900583ce134a79ee934153e9d5
--precious-Adeoye <contact.preciousadeoye>
2025-02-03 10:30:18 -0500

    This is the module 2

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
@@ -45,8 +46,11 @@ app.use(pinoHttp({ logger }));
 // Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
 //{{insert code here}}
 
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
+
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
+
 
 
 // Global Error Handler
@@ -54,6 +58,8 @@ app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
+
+
 
 app.get("/",(req,res)=>{
     res.send("Inside the server")
--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -6,7 +6,8 @@ const MongoClient = require('mongodb').MongoClient;
 let url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
-const dbName = `${process.env.MONGO_DB}`;
+const dbName ="secondChance"
+// `${process.env.MONGO_DB}`;
 
 async function connectToDatabase() {
     if (dbInstance){
@@ -16,13 +17,13 @@ async function connectToDatabase() {
     const client = new MongoClient(url);      
 
     // Task 1: Connect to MongoDB
-    // {{insert code}}
+        await client.connect()
 
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+         dbInstance =  client.db(dbName);
 
     // Task 3: Return database instance
-    // {{insert code}}
+        return dbInstance
 }
 
 module.exports = connectToDatabase;
