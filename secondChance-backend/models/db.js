e5351697e7287c872746344024e44e2977fc5e11
Precious-Adeoye <contact.preciousadeoye@gmail.com>
2025-02-07 20:31:30 +0100

    Update db.js

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -2,29 +2,22 @@
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
-
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
-const dbName ="secondChance"
-// `${process.env.MONGO_DB}`;
+const dbName = "secondChance";
 
 async function connectToDatabase() {
     if (dbInstance){
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-        await client.connect()
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-         dbInstance =  client.db(dbName);
-
-    // Task 3: Return database instance
-        return dbInstance
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
